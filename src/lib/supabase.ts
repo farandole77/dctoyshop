825703653d054c4c6c410b0e5b1c620f4ac8e9a2
farandole77 <liechan77@gmail.com>
2026-08-26 17:00:03 +0900

    Add files via upload

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://egfziuejerovmnlahjgn.supabase.co'
-const supabaseKey = 'sb_publishable_T102yh_CtOxOY31WhCjz4w_hREpsCMJ'
+export const supabaseUrl = 'https://egfziuejerovmnlahjgn.supabase.co'
+export const supabaseKey = 'sb_publishable_T102yh_CtOxOY31WhCjz4w_hREpsCMJ'
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
